e43ef3a9f0b3a54f2fcb2f6c88186a4323da82b7
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-03 15:23:27 -0500

    fleshing out project items, links and details

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -20,26 +20,82 @@ export const Projects = (props, direction) => {
 
                         <div className="row projects-row">
                             <div className="col-12 project-item">
-                                <img src="/images/innovate.png" alt="project thumb" />
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                                    culpa qui officia deserunt mollit anim id est laborum.
-                                </p>
+
+                                <div className="project-item-img">
+                                    <a href="https://quiet-basin-68498.herokuapp.com/" target="_blank" rel="noreferrer">
+                                        <img src="/images/innovate.png" alt="project thumb" />
+                                    </a>
+                                </div>
+
+                                <div className="project-info">
+                                    <h3>Innovate Birmingham Candidate Portal</h3>
+                                    <p>
+                                        This was a group project I worked on as a student. I helped organize the project from start to finish and 
+                                        worked primarily on front end functionality, UI/UX and design. I also worked with the back
+                                        end crew to design our SQL database. 
+                                        {/* built our landing, home and profile pages along 
+                                        with the header, footer, and modals throughout.  */}
+                                        <br /> <br /> 
+                                        Login auth is currently disabled... no credentials neccessary. 
+                                        <br />
+                                        Please be patient as the landing page may load slowly on Heroku.
+                                        <br /> <br />
+                                        
+                                        <div className="project-item-links">
+                                            <a href="https://quiet-basin-68498.herokuapp.com/" 
+                                               target="_blank" 
+                                               rel="noreferrer"
+                                            >
+                                                Heroku Link 
+                                            </a>
+                                            |
+                                            <a href="https://github.com/sage-productions/innovate-bham-candidate-portal" 
+                                               target="_blank" 
+                                               rel="noreferrer"
+                                            >
+                                                Project Github 
+                                            </a>
+                                        </div>
+                                    </p>
+                                </div>
+
                             </div>
                             <div className="col-12 project-item">
-                                <img src="/images/netflix.png" alt="project thumb" />
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                                    culpa qui officia deserunt mollit anim id est laborum.
-                                </p>
+                                <div className="project-item-img">
+                                    <a href="https://jandrew78-netflix.netlify.app" target="_blank" rel="noreferrer">
+                                        <img src="/images/netflix.png" alt="project thumb" />
+                                    </a>
+                                </div>
+
+                                <div className="project-info">
+                                    <h3>Notflix - A Netflix Clone</h3>
+                                    <p>
+                                        Functional clone of Netflix including: Styled Components, Custom Hooks, 
+                                        Context, Firebase DB and Authentication on all pages, Protected Routes with
+                                        Auth Listeners, Live Search on the Browse Page, Video Player and More.
+                                        <br /> <br /> 
+                                        Create your own account or login with the following:   
+                                        <br />
+                                        Email: test@test.com and Password: testaccount123
+                                        <br /> <br />
+                                        
+                                        <div className="project-item-links">
+                                            <a href="https://quiet-basin-68498.herokuapp.com/" 
+                                               target="_blank" 
+                                               rel="noreferrer"
+                                            >
+                                                Heroku Link 
+                                            </a>
+                                            |
+                                            <a href="https://github.com/sage-productions/innovate-bham-candidate-portal" 
+                                               target="_blank" 
+                                               rel="noreferrer"
+                                            >
+                                                Project Github 
+                                            </a>
+                                        </div>
+                                    </p>
+                                </div>
                             </div>
                             <div className="col-12 project-item">
                                 <img src="/images/amazon.png" alt="project thumb" />
