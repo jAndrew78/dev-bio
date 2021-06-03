674451ff64fbb609286268b26b0781050cc03c3d
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-02 20:30:32 -0500

    justified the text and re-wrote the bio and interests sections

--- src/components/about-me/index.jsx
+++ src/components/about-me/index.jsx
@@ -6,9 +6,9 @@ import './styles.scss';
 
 export const AboutMe = props => {
 
-    const bio = "I never saw myself as a computer person, but after taking a break from my career to refinish a house, I picked up a book on Python, sat down at my computer and fell in love.\n From there I enrolled in a web development program with Innovate Birmingham, which well exceeded my expectations, and here I am today, looking forward to a career in the local tech industry."
+    const bio = "I never saw myself as a computer person, but after taking a break from the mortgage industry to refinish a house, I picked up a book on Python, sat down at a computer and fell in love.\n During my time in the web development program at Innovate Birmingham (which well exceeded my expectations), I found a community where I belonged and a career path I could enjoy for the rest of my life. \n I can honestly say, I am very much looking forward to a long and rewarding career in the local tech industry."
     // const other = "When I'm not programming, you might find me trekking through Oak Mtn on a bike or on my own two legs depending on my mood. \n I love to cook, do a little abstract painting on the side and am facinated by both psychology and philosophy. \n On the other hand, I'm a grammar nerd who loves math and science (especially physics and biology). I enjoy a good puzzle, love a good book and am an amatuer DM (mostly for my kids)."
-    const other = "When I'm not programming, you'll likely find me hiking or mountain biking around Oak Mountain. I ike to cook, do a little abstract painting on the side and am interested in psychology and philosophy. \n On the other hand, I'm a grammar nerd who loves math and science. I enjoy a good logic puzzle, love a good book and am an amatuer DM (mostly for my kids)."
+    const other = "When I'm not programming, I can often be found hiking or mountain biking around Oak Mountain. I love to cook, do a little abstract painting on the side and am fascinated by psychology and philosophy. \n On the other hand, I'm a big grammar nerd who loves math and science (mostly biology and physics). I enjoy a good logic puzzle, love a good book and occasionally run a game of D&D for my kids. \n I am excited about a career path that will allow me to pursue both my creative and analytical natures."
 
     return (
         <>
@@ -24,9 +24,6 @@ export const AboutMe = props => {
                         </div>
 
                         {/* <div className="col-xl-1 col-lg-none"></div> */}
-
-                        {/* <div className="about-me-bio col-xl-7 col-lg-12 px-5">
-                        </div> */}
 
                         <div className="about-me-bio col-xl-4 col-lg-4 col-md-12">
                             <h3>Developer Bio</h3>
