c4e8b3447516dc068d9440748a1a3cd75bb6460f
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-07 16:53:11 -0500

    set up emailjs in onSubmit

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
+import emailjs from 'emailjs-com';
 import { NavbarStatic, BgStatic } from '../'
 import './styles.scss';
 
@@ -11,36 +10,47 @@ export const Contact = props => {
     const { register, errors, handleSubmit, reset } = useForm();
 
     const onSubmit = async (data) => {
-        console.log('Name: ', data.name);
-        console.log('Email: ', data.email);
-        console.log('Subject: ', data.subject);
-        console.log('Message: ', data.message);
-  };
+        try {
+            const templateParams = {
+                name: data.name,
+                email: data.email,
+                subject: data.subject,
+                message: data.message
+            };
+            await emailjs.send(
+                process.env.REACT_APP_SERVICE_ID,
+                process.env.REACT_APP_TEMPLATE_ID,
+                templateParams,
+                process.env.REACT_APP_USER_ID
+            );
+            reset();
+        } catch (error) {
+          console.log(error);
+        }
+    };
 
-    return (
-        <>
-            <NavbarStatic />
-            <BgStatic />
+    return <>
+        <NavbarStatic />
+        <BgStatic />
 
-            <div className="container-fluid profile-container">
-                <div className="profile-page-bg">
-                    <div className="row profile-row">
+        <div className="container-fluid profile-container">
+            <div className="profile-page-bg">
+                <div className="row profile-row">
 
-                        <div className="profile-section-heading col-lg-3 col-md-12">
-                            <h1 className="profile-section-heading-text">Dev_Contact</h1>
-                        </div>
+                    <div className="profile-section-heading col-lg-3 col-md-12">
+                        <h1 className="profile-section-heading-text">Dev_Contact</h1>
+                    </div>
 
-                        <div className='col-12 text-center'>
-                            <div className='contactForm'>
-                                <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
-                                    
-                                    {/* Row 1 of form */}
-                                    <div className='row formRow'>
-                                        <div className='col-6'>
-                                            <input
+                    <div className='col-12 text-center'>
+                        <div className='contactForm'>
+                            <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                                
+                                {/* Row 1 of form */}
+                                <div className='row formRow'>
+                                    <div className='col-6'>
+                                        <input
                                             type='text'
-                                            name='name'
-                                            ref={register({
+                                            {...register('name', {
                                                 required: { value: true, message: 'Please enter your name' },
                                                 maxLength: {
                                                 value: 30,
@@ -48,34 +58,32 @@ export const Contact = props => {
                                                 }
                                             })}
                                             className='form-control formInput'
-                                            placeholder='Name'
-                                            ></input>
-                                            {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
-                                        </div>
-                                        <div className='col-6'>
-                                            <input
+                                            placeholder='Name'></input>
+                                        {errors && <span className='errorMessage'>{errors.name.message}</span>}
+                                        {console.log(errors)}
+                                    </div>
+                                    <div className='col-6'>
+                                        <input
                                             type='email'
-                                            name='email'
-                                            ref={register({
+                                            {...register('email', {
                                                 required: true,
                                                 pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
                                             })}
                                             className='form-control formInput'
-                                            placeholder='Email address'
-                                            ></input>
-                                            {errors.email && (
-                                            <span className='errorMessage'>Please enter a valid email address</span>
-                                            )}
-                                        </div>
+                                            placeholder='Email address'></input>
+                                        {/* {errors.email && ( */}
+                                        {errors && (
+                                        <span className='errorMessage'>Please enter a valid email address</span>
+                                        )}
                                     </div>
+                                </div>
 
-                                    {/* Row 2 of form */}
-                                    <div className='row formRow'>
-                                        <div className='col'>
-                                            <input
+                                {/* Row 2 of form */}
+                                <div className='row formRow'>
+                                    <div className='col'>
+                                        <input
                                             type='text'
-                                            name='subject'
-                                            ref={register({
+                                            {...register('subject', {
                                                 required: { value: true, message: 'Please enter a subject' },
                                                 maxLength: {
                                                 value: 75,
@@ -83,39 +91,36 @@ export const Contact = props => {
                                                 }
                                             })}
                                             className='form-control formInput'
-                                            placeholder='Subject'
-                                            ></input>
-                                            {errors.subject && (
-                                            <span className='errorMessage'>{errors.subject.message}</span>
-                                            )}
-                                        </div>
+                                            placeholder='Subject'></input>
+                                        {/* {errors.subject && ( */}
+                                        {errors && (
+                                        <span className='errorMessage'>{errors.subject.message}</span>
+                                        )}
                                     </div>
+                                </div>
 
-                                    {/* Row 3 of form */}
-                                    <div className='row formRow'>
-                                        <div className='col'>
-                                            <textarea
+                                {/* Row 3 of form */}
+                                <div className='row formRow'>
+                                    <div className='col'>
+                                        <textarea
                                             rows={3}
-                                            name='message'
-                                            ref={register({
+                                            {...register('message', {
                                                 required: true
                                             })}
                                             className='form-control formInput'
-                                            placeholder='Message'
-                                            ></textarea>
-                                            {errors.message && <span className='errorMessage'>Please enter a message</span>}
-                                        </div>
+                                            placeholder='Message'></textarea>
+                                        {errors && <span className='errorMessage'>Please enter a message</span>}
                                     </div>
-                                    <button className='submit-btn' type='submit'>
-                                    Submit
-                                    </button>
-                                </form>
-                            </div>
+                                </div>
+                                <button className='submit-btn' type='submit'>
+                                Submit
+                                </button>
+                            </form>
                         </div>
-
                     </div>
+
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </>;
 }
